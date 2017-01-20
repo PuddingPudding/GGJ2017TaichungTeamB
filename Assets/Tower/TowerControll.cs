15434309fdcb8fb67912c997500187c15243c7b3
PuddingPudding <hwsh70119@gmail.com>
2017-01-21 02:51:48 +0800

    新增 Tower 資料夾，裡頭裝的是防禦塔的最基本原型，程式碼跟圖片

---
+++ Assets/Tower/TowerControll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerControll : MonoBehaviour
+{
+    public GameObject wave;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject waveCopy = null;
+            waveCopy = Instantiate(wave, transform.position, new Quaternion(0, 0, 0, 0));
+        }
+    }
+}
