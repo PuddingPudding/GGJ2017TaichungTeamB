b2e4ce6dc2d18fe5e158c3fb0ac10b08addcacbc
fred30920444 <fred309200444@gmail.com>
2017-01-21 06:56:21 +0800

    加了background 有設layer&sorting layer  script中有 抓時間的object

---
+++ Assets/Back Ground/background.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class background : MonoBehaviour {
+    public object dateTime;
+
+    // Use this for initialization
+    void Start () {
+        dateTime = DateTime.Now.Hour;//抓時間
+        
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
