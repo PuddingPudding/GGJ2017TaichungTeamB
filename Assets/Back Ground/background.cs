ed6eb818056a06d28938bb8cb488c7408d33ce76
fred30920444 <fred309200444@gmail.com>
2017-01-21 08:36:30 +0800

    just commit

--- Assets/Back Ground/background.cs
+++ Assets/Back Ground/background.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class background : MonoBehaviour {
+public class background : MonoBehaviour
+{
     public object dateTime;
+    public GameObject Emeny;
+    public float time;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         dateTime = DateTime.Now.Hour;//抓時間
-        
+
 
 
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime; //時間增加
+        if (time > 0.4) //如果時間大於0.5(秒)
+        {
+            Vector3 pos = new Vector3(UnityEngine.Random.Range(-1.5f, 1.5f), UnityEngine.Random.Range(-1f, 1f), 0); //宣告位置pos，Random.Range(-2.5f,2.5f)代表X是2.5到-2.5之間隨機
+            Instantiate(Emeny, pos, transform.rotation); //產生敵人
+            time = 0f; //時間歸零
+        }
+
+    }
 }
