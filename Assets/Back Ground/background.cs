2df28760b5e2b3104b9f094ea52ba8072fb7f723
fred30920444 <fred309200444@gmail.com>
2017-01-21 08:41:19 +0800

    敵人會生成在周圍 並向中間靠

--- Assets/Back Ground/background.cs
+++ Assets/Back Ground/background.cs
@@ -26,7 +26,7 @@ public class background : MonoBehaviour
         time += Time.deltaTime; //時間增加
         if (time > 0.4) //如果時間大於0.5(秒)
         {
-            Vector3 pos = new Vector3(UnityEngine.Random.Range(-1.5f, 1.5f), UnityEngine.Random.Range(-1f, 1f), 0); //宣告位置pos，Random.Range(-2.5f,2.5f)代表X是2.5到-2.5之間隨機
+            Vector3 pos = new Vector3(UnityEngine.Random.Range(-3f, 3f), UnityEngine.Random.Range(-2f, 2f), 0); //宣告位置pos，Random.Range(-2.5f,2.5f)代表X是2.5到-2.5之間隨機
             Instantiate(Emeny, pos, transform.rotation); //產生敵人
             time = 0f; //時間歸零
         }
