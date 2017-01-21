d7a7da75616e1a9a496e2ffbefdce960a2347c72
PuddingPudding <hwsh70119@gmail.com>
2017-01-21 09:06:31 +0800

    將敵人的生成調為正常範圍，現在不會一堆小兵生在防禦塔周圍了

--- Assets/Back Ground/background.cs
+++ Assets/Back Ground/background.cs
@@ -8,15 +8,12 @@ public class background : MonoBehaviour
     public object dateTime;
     public GameObject Emeny;
     public float time;
+    public float initRange = 6; //生成敵人的範圍
 
     // Use this for initialization
     void Start()
     {
         dateTime = DateTime.Now.Hour;//抓時間
-
-
-
-
 
     }
 
@@ -24,9 +21,18 @@ public class background : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime; //時間增加
-        if (time > 0.4) //如果時間大於0.5(秒)
+        if (time >= 0.5f) //如果時間大於0.5(秒)
         {
-            Vector3 pos = new Vector3(UnityEngine.Random.Range(-3f, 3f), UnityEngine.Random.Range(-2f, 2f), 0); //宣告位置pos，Random.Range(-2.5f,2.5f)代表X是2.5到-2.5之間隨機
+            float IR = initRange;
+            Vector3 pos = new Vector3(UnityEngine.Random.Range(-2 * IR, 0), UnityEngine.Random.Range(-2 * IR, 0), 0); //宣告位置pos，Random.Range(-2.5f,2.5f)代表X是2.5到-2.5之間隨機
+            if (pos.x > -1 * IR)
+            {
+                pos.x += 2 * IR;
+            }
+            if (pos.y > -1 * IR)
+            {
+                pos.y += 2 * IR;
+            }
             Instantiate(Emeny, pos, transform.rotation); //產生敵人
             time = 0f; //時間歸零
         }
