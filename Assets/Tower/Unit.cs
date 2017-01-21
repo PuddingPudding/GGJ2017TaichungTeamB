295d6022424a98368912e469729a5ac10d992754
PuddingPudding <hwsh70119@gmail.com>
2017-01-21 08:13:25 +0800

    Tower原型，目前防禦塔可以發射簡單的波形，敵人會自動靠近防禦塔

---
+++ Assets/Tower/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string UnitName = "New Unit";//名稱預設為新單位
+    public int HP = 10;//生命值預設為10
+    public float Speed = 2;//速度預設為2
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
